aa3968e29546c5c13e7e0c94f98885b23b2bf37f
eVolk <evgeny.volk@mysky.com>
2019-10-18 17:35:31 +0300

    commit

---
+++ precache-manifest.401c0295862fd234380b1b70c08fad27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa1d6fb70ff8333986722b66cf89901",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d432387b29c274cb35af",
+    "url": "/static/css/main.f3d21ce9.chunk.css"
+  },
+  {
+    "revision": "905346017c44b94ab81e",
+    "url": "/static/js/2.fe75b73b.chunk.js"
+  },
+  {
+    "revision": "d432387b29c274cb35af",
+    "url": "/static/js/main.6cc5842d.chunk.js"
+  },
+  {
+    "revision": "b61caf1d1ee57cdf38e7",
+    "url": "/static/js/runtime~main.fc70d7da.js"
+  },
+  {
+    "revision": "f5ff58f50a4f35734888289092c05359",
+    "url": "/static/media/1.f5ff58f5.png"
+  },
+  {
+    "revision": "261308271cd7656716f28df430a532bc",
+    "url": "/static/media/promo.26130827.jpeg"
+  }
+]);
