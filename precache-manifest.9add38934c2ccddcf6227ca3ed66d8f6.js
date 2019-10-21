4a4d88b97dac4cb2d1060f1d4279555aac68ca01
eVolk <evgeny.volk@mysky.com>
2019-10-21 15:27:38 +0300

    Updates

---
+++ precache-manifest.9add38934c2ccddcf6227ca3ed66d8f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ab27072e420094d9b198f139af14d6",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "7e30cc6dc03d416718fb",
+    "url": "/shop/static/css/main.d4801e2f.chunk.css"
+  },
+  {
+    "revision": "b61d5a3858b458d2cd61",
+    "url": "/shop/static/js/2.dbce4b26.chunk.js"
+  },
+  {
+    "revision": "7e30cc6dc03d416718fb",
+    "url": "/shop/static/js/main.45374b8d.chunk.js"
+  },
+  {
+    "revision": "be7aea1292cbf788ac56",
+    "url": "/shop/static/js/runtime~main.7ef0fc46.js"
+  },
+  {
+    "revision": "f5ff58f50a4f35734888289092c05359",
+    "url": "/shop/static/media/1.f5ff58f5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4bd4959f1d4882bcdebcc30276e5c64a.js"
+  "/shop/precache-manifest.9add38934c2ccddcf6227ca3ed66d8f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
